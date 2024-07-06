c4719c9db715ab7f4ee1496b969be59ef91ceb8c
alexArias96 <alexherdez3092@gmail.com>
2024-07-06 00:47:43 -0600

    FRONT-ECOM-ARIAS-06

--- src/app/customer/services/customer.service.ts
+++ src/app/customer/services/customer.service.ts
@@ -41,6 +41,13 @@ export class CustomerService {
     });
   }
 
+  applyCoupon(code:any):Observable<any>{
+    const userId = UserStorageService.getUserId(); 
+    return this.http.get(BASIC_URL + `api/customer/coupon/${userId}/${code}`, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
   private createAuthorizationHeader(): HttpHeaders{
     return new HttpHeaders().set(
       'Authorization', 'Bearer ' + UserStorageService.getToken()
